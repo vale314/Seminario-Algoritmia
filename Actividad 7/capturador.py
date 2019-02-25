0e6a4f73acf908708e7c74e870c20d724cbe28e1
vale314 <electrovale314@hotmail.com>
2019-02-25 17:55:51 -0600

    Actividad 06

---
+++ Actividad 7/capturador.py
@@ -0,0 +1,46 @@
+import json
+from PySide2.QtWidgets import QMainWindow, QMessageBox,QFileDialog,QGraphicsScene
+from ui_mainwindow import Ui_MainWindow
+from PySide2.QtGui import QPen, QColor,QBrush
+from particula import Particula
+import math
+
+class Capturador:
+    def __init__(self):
+        self.lista=[]
+
+    def agregar(self, particula):
+        self.lista.append(particula)
+
+    def mostrar(self):
+
+        for particula in self.lista:
+            print(particula)
+
+    def guardar(self, file,pen,scene):
+        scene.addLine(0, 0, 499, 0, pen)
+        lista_dicc = []
+        for particula in self.lista:
+            lista_dicc.append(particula.to_dicc())
+
+        with open(file, 'w') as archivo:
+           json.dump(lista_dicc, archivo, indent=5)
+
+
+    def recuperar(self, file):
+        with open(file, 'r') as archivo:
+            particulas= json.load(archivo)
+
+            for particula in particulas:
+                p = Particula()
+                p.id = particula['id']
+                p.origenX = particula['origen']['x']
+                p.origenY = particula['origen']['y']
+                p.destinoX = particula['destino']['x']
+                p.destinoY = particula['destino']['y']
+                p.distancia = math.sqrt(pow((int(p.destinoX)-int(p.origenX)),2)+pow((int(p.destinoY)-int(p.origenY)),2))
+                p.velocidad = particula['velocidad']
+                p.red = particula['color']['red']
+                p.green = particula['color']['green']
+                p.blue = particula['color']['blue']
+                self.lista.append(p)
